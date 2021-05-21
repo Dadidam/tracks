8befe4612fbcdb73733f249aabc2bc546401cfca
Ilia Vorontcov <iliav@hurdit.com>
2021-05-21 16:23:55 -0400

    Fetch and parse JSON, dispaly some data on the page

---
+++ fetch.js
@@ -0,0 +1,27 @@
+function fetchTracks() {
+  const app = document.querySelector("#app");
+  app.innerHTML = "Loading...";
+
+  fetch("tracks/top.json")
+    .then(res => {
+      if (!res.ok) {
+        throw Error("Fetching data error");
+      }
+
+      return res.json();
+    })
+    .then(data => {
+      const html = data.tracks
+        .map(track => `<p>Title: ${track.title}</p>`)
+        .join("");
+
+      // get rid of "preloader"
+      app.innerHTML = "";
+
+      // update html layout with a list of tracks
+      app.insertAdjacentHTML("afterbegin", html);
+    })
+    .catch(error => console.error(error));
+}
+
+fetchTracks();
